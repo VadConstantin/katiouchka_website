7b8afda055317468c726ac80573ae926aba66b3c
Vadim <vad.constantin@gmail.com>
2025-02-22 12:29:10 +0100

    next config updated with env variables

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,11 @@ const nextConfig: NextConfig = {
   compiler: {
     styledComponents: true, 
   },
+  env: {
+    NEXT_PUBLIC_CONTENTFUL_SPACE_ID: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+    NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+  },
 };
 
 export default nextConfig;
+
